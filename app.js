2a622a5bcbfc63bee043ab9927b5e2f9ab89e332
CSU-Ryan <enderbro@colostate.edu>
2026-04-20 21:11:53 -0600

    Step 5

--- app.js
+++ app.js
@@ -4,18 +4,6 @@ function update_active_color(new_color = active_color) {
     $(`.strip-cell.${active_color}`).addClass("selected");
 }
 
-function alert_coordinate(cell_coord) {
-    let x = parseInt(cell_coord.substring(4,6));
-    let y = parseInt(cell_coord.substring(6));
-
-    alert(`Row ${x}, Col ${y}`);
-}
-
-function apply_color(cell) {
-    cell.removeClass();
-    cell.addClass(`game-cell ${active_color}`);
-}
-
 $(document).ready(function () {
 
     $(".strip-cell").click(function () {
@@ -23,8 +11,6 @@ $(document).ready(function () {
     });
 
     $(".game-cell").click(function () {
-        alert_coordinate($(this).attr("id"));
-        apply_color($(this));
         $(this).removeClass().addClass(`game-cell ${active_color}`);
     });
 
