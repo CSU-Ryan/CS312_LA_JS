c574edb7a86dc5e36ed9361b5c19bce8ac305b55
CSU-Ryan <enderbro@colostate.edu>
2026-04-21 15:03:49 -0600

    Step 9

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 function update_active_color(new_color = active_color) {
+    // Updates all cells to reflect new color.
+    $(`.game-cell.${active_color}`)
+        .removeClass(active_color).addClass(new_color);
+
+    // Updates color picker
     $(".strip-cell.selected").removeClass("selected");
     active_color = new_color;
     $(`.strip-cell.${active_color}`).addClass("selected");
+
 }
 
 function toggle_color(obj) {
@@ -14,6 +20,7 @@ function toggle_color(obj) {
         cell.addClass(active_color);
     }
 }
+
 
 $(document).ready(function () {
 
